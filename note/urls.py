0b36a6d5210adee603fe7339f045d184b3de1d04
khanTaufeeq <taufeeqyouth@gmail.com>
2024-03-03 12:01:00 +0530

    updated views and urls

--- note/urls.py
+++ note/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.ListAllNotes.as_view()),
     path('add/',views.CreateNewNote.as_view()),
     path('<int:id>/', views.FetchOneNote.as_view()),
-    path('<int:id>/', views.UpdateOneNote.as_view())
+    path('update/<int:pk>/', views.UpdateOneNote.as_view()),
+    path('search/', views.FetchNoteSubString.as_view(), name='note-search')
 ]
 
--- note/views.py
+++ note/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from . serializer import NoteSerializer
 from . models import Note
 from rest_framework import generics 
-from rest_framework.mixins import ListModelMixin
+from rest_framework.views import APIView
+# from rest_framework.filters import SearchFilter
+# from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response 
+from rest_framework import status
 # Create your views here.
 
 
@@ -28,4 +32,31 @@ class FetchOneNote(generics.RetrieveAPIView):
 class UpdateOneNote(generics.UpdateAPIView):
 
     queryset = Note.objects.all()
-    serializer_class = NoteSerializer
+    serializer_class = NoteSerializer 
+
+    def put(self, request, pk):
+        response = super().put(request, pk)
+        return response
+
+
+class FetchNoteSubString(APIView):
+
+    def get(self, request, format=None):
+        substring = request.query_params.get('title', None) 
+
+        if substring:
+            notes = Note.objects.filter(title__icontains = substring) 
+        else:
+            notes = Note.objects.all() 
+
+        serializer = NoteSerializer(notes, many=True)
+
+        return Response(serializer.data , status = status.HTTP_200_OK ) 
+
+    # queryset = Note.objects.all() 
+    # serializer = NoteSerializer 
+    # search_backends = [SearchFilter]
+    # search_field = ['title']
+
+
+
